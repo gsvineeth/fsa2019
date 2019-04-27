ec37eed54b3631bfb5b9952880e231113c219bf3
web2feel <web2feel@gmail.com>
2019-04-27 12:07:07 +0530

    chnages

--- page.php
+++ page.php
@@ -15,7 +15,7 @@
 get_header();
 ?>
 <div class="container"><div class="row"> 
-	<div class="col-md-8"> 
+	<div class="col-md-12"> 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
@@ -36,9 +36,9 @@ get_header();
 		</main><!-- #main -->
 	</div><!-- #primary -->
 	</div>
-	<div class="col-md-4"> 
-	<?php get_sidebar(); ?>
-	</div>
+	<!-- <div class="col-md-4"> 
+	<?php //get_sidebar(); ?>
+	</div> -->
 </div></div>
 <?php
 get_footer();
